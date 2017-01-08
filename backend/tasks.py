aea3a2e89de7add84d40f4fab88883fdec08b53c
Michael Killough <michaeljkillough@gmail.com>
2017-01-08 23:13:40 +0000

    Check for cancelation in dummy task.

--- backend/tasks.py
+++ backend/tasks.py
@@ -12,7 +12,12 @@ def dummy_sleeping_task(task, total_time_in_seconds):
     sleep_period = 2 # seconds
     total_sleep_ticks = math.ceil(total_time_in_seconds / sleep_period)
     for tick in range(total_sleep_ticks):
+        # Check if we've been canceled. Will raise an exception if we have,
+        # which the task runner will catch and handle.
+        task.throw_if_canceled()
+
         progress = (tick / total_sleep_ticks) * 100
         task.set_progress(progress)
         print('Tick %i: %i' % (task.id, progress))
+
         time.sleep(sleep_period)
