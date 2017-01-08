ccb4e5383db66271946618b082bad50394220b1f
Michael Killough <michaeljkillough@gmail.com>
2017-01-08 21:25:20 +0000

    Tweak dummy task, so it actually sleeps.
    Does not yet update progress, as we've not passed the TaskContext
    through to the task.

--- backend/backend/tasks.py
+++ backend/backend/tasks.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import math
+import time
+
 import taskqueue
 
 
 @taskqueue.task()
-def dummy_task():
-    print('hi')
+def dummy_sleeping_task(total_time_in_seconds):
+    sleep_period = 2 # seconds
+    total_sleep_ticks = math.ceil(total_time_in_seconds / sleep_period)
+    for tick in range(total_sleep_ticks):
+        print('tick')
+        time.sleep(sleep_period)
--- backend/backend/views.py
+++ backend/backend/views.py
@@ -7,5 +7,7 @@ from . import tasks
 
 
 def initiate_dummy_task(request):
-    tasks.dummy_task.call_async()
+    # TODO: Should really @require_POST as this view has side-effects. However,
+    #       to make it easier to test via the browser, we'll leave it off.
+    tasks.dummy_sleeping_task.call_async(30)
     return JsonResponse(dict(success=True))
