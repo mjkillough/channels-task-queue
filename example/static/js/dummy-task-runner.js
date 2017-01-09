614dda79dde422f3737d12092dc353d4a8fb2ee2
Michael Killough <michaeljkillough@gmail.com>
2017-01-09 22:11:23 +0000

    Add dummy-task-runner component.
    A simple Vuejs component that starts a task, then polls the server to
    find out the current status of the task. When the task has finished
    (canceled/failed/completed), then the user can start a new task to
    replace it. Progress is currently shown by dumping the JSON response we
    get from the server.
    
    This code uses ES6/7 features which aren't supported by all browsers.
    Only tested in Chrome 55, and I expect it'll only work in Chrome 55 or
    Edge, due to the use of `await`/`async`. (If this were a serious
    project, I'd bother setting up Babel, honest!)

---
+++ example/static/js/dummy-task-runner.js
@@ -0,0 +1,69 @@
+'use strict';
+
+(function() {
+
+    var TASK_STATUS = {
+        Queued: 1,
+        Running: 2,
+        Canceled: 3,
+        Failed: 4,
+        Complete: 5,
+    };
+
+    // States that indicate the task is running (or about to run) and its
+    // status may change.
+    var TASK_PENDING_STATES = [
+        TASK_STATUS.Queued, TASK_STATUS.Running,
+    ];
+
+    // <dummy-task-runner> component, which allows a user to start our task,
+    // watch its progress and cancel its execution.
+    Vue.component('dummy-task-runner', {
+        template: '#tmpl-dummy-task-runner',
+
+        data: () => {
+            return {
+                pollIntervalMs: 100,
+                task: null,
+            };
+        },
+
+        computed: {
+            havePendingTask: function() {
+                return (
+                    this.task !== null &&
+                    TASK_PENDING_STATES.includes(this.task.status)
+                );
+            },
+        },
+
+        methods: {
+            startTask: async function() {
+                const resp = await fetch('/start/');
+                const json = await resp.json();
+                console.assert(json.success);
+                this.task = json.task;
+
+                // Set off our polling of task status, which will continue
+                // until the task is no longer pending.
+                setTimeout(this.pollTaskStatus, this.pollIntervalMs);
+            },
+
+            pollTaskStatus: async function() {
+                if (!this.havePendingTask) {
+                    return;
+                }
+
+                const resp = await fetch('/task/' + this.task.id + '/');
+                const json = await resp.json();
+                console.assert(json.success);
+                this.task = json.task;
+
+                // Call ourselves again after poll interval. If we find
+                // ourselves without a pending task, we'll stop updating.
+                setTimeout(this.pollTaskStatus, this.pollIntervalMs);
+            },
+        },
+    });
+
+})();
